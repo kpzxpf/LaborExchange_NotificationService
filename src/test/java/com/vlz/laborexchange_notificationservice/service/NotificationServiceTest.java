40afe4461efedc25d3d4ff5f090c7544071fa894
Nikita Volzhin <vlzh.nik@mail.ru>
2026-03-21 17:32:34 +0500

    Update test

--- src/test/java/com/vlz/laborexchange_notificationservice/service/NotificationServiceTest.java
+++ src/test/java/com/vlz/laborexchange_notificationservice/service/NotificationServiceTest.java
@@ -12,16 +12,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-
 import java.util.Map;
 
 @ExtendWith(MockitoExtension.class)
 class NotificationServiceTest {
 
     @Mock
-    private JavaMailSender mailSender;
+    private EmailSender emailSender;
 
     @Mock
     private NotificationProperties properties;
@@ -51,10 +48,7 @@ class NotificationServiceTest {
         notificationService.createEmail(event);
 
         // Assert
-        verify(mailSender).send(argThat((SimpleMailMessage msg) ->
-                msg.getTo()[0].equals(RECIPIENT) &&
-                        msg.getText().equals("Body for Java Developer")
-        ));
+        verify(emailSender).send(RECIPIENT, "Subject", "Body for Java Developer");
     }
 
     @Test
@@ -69,6 +63,6 @@ class NotificationServiceTest {
         notificationService.createEmail(event);
 
         // Assert
-        verify(mailSender, never()).send(any(SimpleMailMessage.class));
+        verify(emailSender, never()).send(any(), any(), any());
     }
 }
